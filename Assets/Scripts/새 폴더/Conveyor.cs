200ae62fe8a3d4d741418a805a06f60a313ed550
HYUNSU4886 <167495966+HYUNSU4886@users.noreply.github.com>
2024-06-04 16:07:11 +0900

    ex

---
+++ Assets/Scripts/새 폴더/Conveyor.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public Transform Box;
+    public Vector3 direction;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Box"))
+        {
+            Box = other.transform;
+            StartCoroutine(Moving(direction, speed));
+        }
+    }
+
+    IEnumerator Moving(Vector3 direction, float speed)
+    {
+        while (true)
+        {
+            Box.GetComponent<Rigidbody>().velocity = direction * speed;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Box"))
+        {
+            
+        }
+    }
+}
