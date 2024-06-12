43ed1aa2071fc027e21c7c6b165f7513d6e7fa88
leehyeukJin <leehjin@icloud.com>
2024-06-12 15:23:29 +0900

    24.06.12
    GG

--- Assets/Scripts/LoadingCylinder.cs
+++ Assets/Scripts/LoadingCylinder.cs
@@ -7,8 +7,9 @@ using UnityEngine.UI;
 
 public class LoadingCylinder : MonoBehaviour
 {
-
     public Transform piston;
+    public Transform box;  // Box의 Transform을 추가합니다.
+    public string boxLayerName = "Box"; // 할당할 레이어 이름
     Vector3 Origin;
     public Button active;
     public Button reverse;
@@ -24,6 +25,30 @@ public class LoadingCylinder : MonoBehaviour
     public int sensing;
     public float location;
 
+    void Awake()
+    {
+        // 레이어 이름을 통해 레이어 번호를 가져옴
+        int boxLayer = LayerMask.NameToLayer(boxLayerName);
+
+        // 모든 게임 오브젝트를 검색
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+
+        // 특정 레이어를 가진 첫 번째 게임 오브젝트를 찾아 box 변수에 할당
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.layer == boxLayer)
+            {
+                box = obj.transform;
+                break;
+            }
+        }
+
+        if (box == null)
+        {
+            Debug.LogError($"Box object in layer '{boxLayerName}' not found!");
+        }
+    }
+
     void Start()
     {
         PLCInput1 = '0';
@@ -33,26 +58,9 @@ public class LoadingCylinder : MonoBehaviour
         isPistonMoving = 0;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (PLCInput1 == '1')
-        {
-            if(isPistonMoving == 0)
-            {
-                isPistonMoving = 1;
-                StartCoroutine(FrontPLCPistons());
-            }
-        }
-        if (PLCInput2 == '1')
-        {
-            if (isPistonMoving == 0)
-            {
-                isPistonMoving = 1;
-                StartCoroutine(BackPLCPistons());
-            }
-        }
-
+        // 트리거 기반으로 업데이트 로직을 대체하므로, 여기서는 필요 없습니다.
     }
 
     public void OnActivePistonBtnClkEvent()
@@ -60,13 +68,36 @@ public class LoadingCylinder : MonoBehaviour
         Origin = piston.position;
         time = 0;
         print("Activate Cylinder");
+
+        if (box != null)
+        {
+            // Box를 piston의 자식으로 설정합니다.
+            box.SetParent(piston);
+        }
+        else
+        {
+            Debug.LogError("Box is not assigned!");
+        }
+
         StartCoroutine(Pistons(direction, speed, distance));
     }
+
     public void OnReversePistonBtnClkEvent()
     {
         Origin = piston.position;
         time = 0;
         print("Reverse Activate Cylinder");
+
+        if (box != null)
+        {
+            // Box를 piston의 자식에서 해제합니다.
+            box.SetParent(null);
+        }
+        else
+        {
+            Debug.LogError("Box is not assigned!");
+        }
+
         StartCoroutine(Pistons(-direction, speed, distance));
     }
 
@@ -77,26 +108,27 @@ public class LoadingCylinder : MonoBehaviour
 
     IEnumerator FrontPLCPistons()
     {
-        if(location <= distance) 
-        { 
+        while (location <= distance)
+        {
             location = location + _direction * speed;
             piston.position = piston.position + direction * speed;
             yield return new WaitForSeconds(0.01f);
         }
         isPistonMoving = 0;
     }
+
     IEnumerator BackPLCPistons()
     {
-        if (location >= 0)
+        while (location >= 0)
         {
-
             location = location - _direction * speed;
             piston.position = piston.position - direction * speed;
             yield return new WaitForSeconds(0.01f);
         }
         isPistonMoving = 0;
     }
-    IEnumerator Pistons(Vector3 direction,float speed,float distance)
+
+    IEnumerator Pistons(Vector3 direction, float speed, float distance)
     {
         active.interactable = false;
         reverse.interactable = false;
@@ -110,10 +142,10 @@ public class LoadingCylinder : MonoBehaviour
             piston.position = Vector3.Lerp(Origin, Origin + distance * direction, time * speed / distance);
             yield return new WaitForSeconds(0.01f);
         }
-        if(sensing == 1)
+        if (sensing == 1)
         {
             sensing = 0;
-            while(true)
+            while (true)
             {
                 time -= 0.01f;
                 if (time <= 0)
@@ -125,5 +157,21 @@ public class LoadingCylinder : MonoBehaviour
         reverse.interactable = true;
         active.interactable = true;
         endIndex = 1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerArea"))
+        {
+            PLCInput1 = '1'; // 트리거가 감지되면 PLCInput1을 '1'로 설정
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("TriggerArea"))
+        {
+            PLCInput1 = '0'; // 트리거가 벗어나면 PLCInput1을 '0'로 설정
+        }
     }
 }
