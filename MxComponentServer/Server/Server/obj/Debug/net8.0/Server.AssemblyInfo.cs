730fff7db6308233374cca795aa5b89c14bb2061
HIIMMMMM <167495786+HIIMMMMM@users.noreply.github.com>
2024-06-19 14:22:08 +0900

    24.06.19

--- MxComponentServer/Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ MxComponentServer/Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23434e8f2f824753ff0ad5aad85cdf5c6247c615")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295e4ac08a44a8480e19ba6facf9ace5b63a7c13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
